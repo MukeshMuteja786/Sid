09ba02b9b88793c3760704d642621527baad0e5a
Maneet Matharu <maneet@adibackoffice.com>
2016-08-23 10:25:42 +0530

    one vc changed

--- Sam/ViewController.swift
+++ Sam/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("SidSam")
+        print("Mukesh")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
